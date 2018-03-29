0c63d393112a315e363c26ca7ce6ac90bfd86af7
anton-sturluson <houseyoo@gmail.com>
2018-03-29 18:53:23 -0400

    merge conflict

--- codegen.sh
+++
@@ -1,4 +0,0 @@
-java -cp lib/*:libsempre/* edu.stanford.nlp.sempre.Main -Main.interactive true -Grammar.inPaths grammar/stat.grammar \
--JavaExecutor.classPathPrefix edu.stanford.nlp.sempre.executors.Codegen -FeatureExtractor.featureDomains span \
--LanguageAnalyzer.languageAnalyzer corenlp.CoreNLPAnalyzer -Builder.executor FormulaMatchExecutor \
--Dataset.inPaths train:input/codegen.dataset -Learner.maxTrainIters 3 
